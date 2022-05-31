bae63a239925128656157ec02d912eadda52aec8
fadayopaul <paulfadayo@yahoo.com>
2022-05-31 15:57:38 +0100

    updated

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float: {number:.2f}")
+# print(f"Float: {number:.2f}")
+# print("{:.2f}".format(number))
+# a = print(f"{number:.2f}")
+print(f"{number}")
